099557cf98201c158f10911a06ace1f94723d49e
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2020-05-07 02:17:11 +0530

    MainScreen Done

--- lib/Tabs/Hometab.dart
+++ lib/Tabs/Hometab.dart
@@ -56,47 +56,12 @@ class _HomeTabState extends State<HomeTab> {
             height: size.height*0.025,
           ),
           ExpandedTile(),
-          Container(
-            margin: new EdgeInsets.symmetric(horizontal: 15,vertical: 10),
-            height: size.height*0.13,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Color.fromRGBO(0, 0, 0,.15),
-                  blurRadius: 4.0, // has the effect of softening the shadow
-                  spreadRadius: 2.0, // has the effect of extending the shadow
-                  offset: Offset(
-                    0.0, // horizontal, move right 1
-                    4.0, // vertical, move down 4
-                  ),
-                ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Text("Store Name",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 19),),
-                      Spacer(),
-                      Text("May 4 2020, 02:35 PM",style: TextStyle(fontSize: 15),),
-                    ],
-                  ),
-                  Spacer(),
-                  Row(
-                    children: <Widget>[
-                      Text("Moyalali Name",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),),
-                      Spacer(),
-                      Text("₹150",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),),
-                    ],
-                  )
-                ],
-              ),
-            ),
-          )
+          ExpandedTile(),
+          ExpandedTile(),
+          ExpandedTile(),
+          ExpandedTile(),
+          ExpandedTile(),
+
         ],
       ),
     );
@@ -108,45 +73,62 @@ class ExpandedTile extends StatelessWidget {
   var size;
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context).copyWith(dividerColor: Colors.transparent,splashColor: Colors.transparent,  highlightColor: Colors.transparent);
     size = MediaQuery.of(context).size;
     return Card(
       shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(15.0))),
       elevation: 3,
       margin: new EdgeInsets.symmetric(horizontal: 15,vertical: 10),
-      child: ExpansionTile(
-        backgroundColor: Colors.white,
-        title: _buildTitle(),
-        trailing: SizedBox(),
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: <Widget>[
-                Text("Herzlich Willkommen"),
-                Spacer(),
-                Icon(Icons.check),
-              ],
-            ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Theme(
+          data: theme,
+          child: ExpansionTile(
+            backgroundColor: Colors.white,
+            title: _buildTitle(),
+            trailing: SizedBox(),
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: size.width*0.05),
+                child: Row(
+                  children: <Widget>[
+                    Text("Flat No. 12B, Skyline", style: TextStyle(fontSize: 16),),
+                    Spacer(),
+                    Text("2 KM", style: TextStyle(fontSize: 16),),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: size.width*0.04),
+                child: Row(
+                  children: <Widget>[
+                    Spacer(),
+                    FlatButton(
+                      child: Text("ACCEPT",style: TextStyle(color: Colors.green,fontSize: 19),),
+                      onPressed: (){debugPrint("Accept Pressed");},
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                      highlightColor: Colors.transparent,
+                    ),
+                    FlatButton(
+                      child: Text("REJECT",style: TextStyle(color: Colors.red,fontSize: 19),),
+                      onPressed: (){debugPrint("Reject Pressed");},
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                      highlightColor: Colors.transparent,
+                    )
+                  ],
+                ),
+              )
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: <Widget>[
-                Text("Das Kursmenu"),
-                Spacer(),
-                Icon(Icons.check),
-              ],
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
 
   Widget _buildTitle() {
     return Padding(
-      padding: const EdgeInsets.all(5.0),
+      padding: EdgeInsets.only(left: size.width*0.01,top: size.height*0.02, bottom: size.height*0.02),
       child: Column(
         children: <Widget>[
           Row(
@@ -157,11 +139,11 @@ class ExpandedTile extends StatelessWidget {
             ],
           ),
           SizedBox(
-            height: size.height*0.02,
+            height: size.height*0.025,
           ),
           Row(
             children: <Widget>[
-              Text("Moyalali Name",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),),
+              Text("User Name",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),),
               Spacer(),
               Text("₹150",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19),),
             ],
